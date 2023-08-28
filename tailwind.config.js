8877f5014ed3084be446a6754d9167e0b59704ab
Ishmael Sealey <ishmaelsealey@gmail.com>
2023-08-28 13:02:31 -0400

    began styling page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'mobileHero': "url('/images/bg-intro-mobile.svg')",
+        'desktopHero': "url('/images/bg-intro-desktop.svg')",
+      },
+      colors: {
+        'primaryBlue': 'hsl(223, 26%, 24%)',
+        'primaryGreen': 'hsl(136, 65%, 51%)',
+        'primaryCyan': 'hsl(192, 70%, 51%)',
+        'neutralBlue': 'hsl(233, 8%, 62%)',
+        'neutralLightBlue': 'hsl(220, 16%, 96%)',
+        'neutralGray': 'hsl(0, 0%, 98%)',
+      },
+    },
+    fontFamily: {
+      'serif': '"Public Sans"',
+    },
   },
   plugins: [],
 }
